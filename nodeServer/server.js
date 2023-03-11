3f668d0778a41bd2c605519de5668850774382bc
Majd Harb <97592562+MajdHarbb@users.noreply.github.com>
2023-03-12 01:38:22 +0200

    returing html

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,11 +1,17 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   //set header content type
   res.setHeader("Content-Type", "text/html");
-  res.write("<p>hello, this is a respose</p>");
-  res.end();
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(3000, "localhost", () => {
